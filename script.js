e088dae8da3f5ed4fc27eb697ca1fcaacda52894
afseenk <afseen.khandaker15@gmail.com>
2024-07-03 11:23:12 -0500

    working on values

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const animalButton = document.getElementById('show-animal')
 const powerDiv = document.getElementById('power-div');
 const powerButton = document.getElementById('show-power')
 
+const skillsDiv = document.getElementById('skills-div')
+const skillsButton = document.getElementById('show-skills')
+
 animalButton.addEventListener('click',() => {
     animalDiv.classList.toggle('hidden')
 })
 
 powerButton.addEventListener('click',() => {
     powerDiv.classList.toggle('hidden')
+})
+
+skillsButton.addEventListener('click', () => {
+    skillsDiv.classList.toggle('hidden')
 })
